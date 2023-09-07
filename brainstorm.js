1b73eda3460e2e0ea36888961ca7a494477693ca
goyalg325 <shaffigoyal774@gmail.com>
2023-09-07 19:42:29 +0530

    first commit

---
+++ brainstorm.js
@@ -0,0 +1,6 @@
+const a = 9
+const b = 10
+function addtwo(){
+    console.log(`sum is : ${a+b}`)
+}
+addtwo()
---
+++ names.js
@@ -0,0 +1,7 @@
+// const john = "john"
+// const peter = "peter"
+// const secret = "super secret"
+// module.exports = {john,peter}
+module.exports.person = {
+    name: 'bob'
+}
